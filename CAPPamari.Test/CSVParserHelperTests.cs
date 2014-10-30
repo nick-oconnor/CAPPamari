a5e5e2e85ea9f64bfc74be0cdba45a316e2a8fc5
lieblingskartoffel <carolinewells93@gmail.com>
2014-10-29 20:41:27 -0400

    Added a CSV parser that sort of works
    Also added a test for it

---
+++ CAPPamari.Test/CSVParserHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CAPPamari.Web.Models;
+using CAPPamari.Web.Helpers;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace CAPPamari.Test
+{
+    [TestClass]
+    public class CSVParserHelperTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //take a sample csv file and convert it to a string- this part works
+            var reader = new StreamReader(File.OpenRead(@"testParser.csv"));
+            string input = "";
+            while (!reader.EndOfStream)
+            {
+                input = input + reader.ReadLine();
+            }
+            //parse it
+            IEnumerable<CourseModel> courses = CSVParserHelper.parse(input);
+
+            // and spit out the info
+            foreach (CourseModel course in courses)
+            {
+                Debug.Print("{0} {1}, {2} credits, {3}, {4}", course.DepartmentCode, course.CourseNumber, course.Credits,
+                   course.Semester, course.Grade);
+            }
+        }
+    }
+}
---
+++ CAPPamari.Web/Helpers/CSVParserHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using CAPPamari.Web.Models;
+using CsvHelper;
+
+namespace CAPPamari.Web.Helpers
+{
+    public class CSVParserHelper
+    {
+        public static IEnumerable<CourseModel> parse(string file)
+        {
+            using (var sr = new StringReader(file))
+            {
+                var reader = new CsvReader(sr);
+                //somehow we have to make it delimit the header by newline instead of whatever it's doing
+               
+
+                IEnumerable<CourseModel> courses = reader.GetRecords<CourseModel>();
+
+                return courses;
+            }
+        }
+    }
+}
