fa04e7f5743fcb922d3436b8d7b7f4d845d8a5fa
Nicholas Pitt <ngpitt@iecfusor.com>
2014-11-15 19:21:59 -0500

    Fixed email to advisor

--- CAPPamari.Web/Helpers/EmailHelper.cs
+++ CAPPamari.Web/Helpers/EmailHelper.cs
@@ -16,31 +16,35 @@ namespace CAPPamari.Web.Helpers
         public static bool EmailToAdvisor(string UserName, AdvisorModel Advisor)
         {
             var document = PrintingHelper.PrintCAPPReport(UserName);
-            Attachment report;
+            Attachment attachment;
             MailMessage email = new MailMessage("do-not-reply@iecfusor.com", Advisor.EMail);
             email.Subject = "CAPPamari - Please review " + UserName + "'s CAPP report";
-            email.Body = Advisor.Name + ",\n\n" +
-                    "Please review my latest plan for fulfulling my graduation requirements.\n\n" +
+            email.Body = "Dear " + Advisor.Name + ",\n" +
+                    "\n" +
+                    "Please review my latest plan for fulfulling my graduation requirements.\n" +
+                    "\n" +
                     "Sincerely,\n" +
-                    UserName + "\n\n" +
-                    "Sent on " + UserName + "'s behalf by CAPPamari.";
+                    UserName + "\n" +
+                    "--\n" +
+                    HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority);
             using (var pdfStream = new MemoryStream())
             {
                 document.SaveToStream(pdfStream);
-                report = new Attachment(pdfStream, new ContentType(MediaTypeNames.Application.Pdf));
+                pdfStream.Position = 0;
+                attachment = new Attachment(pdfStream, new ContentType(MediaTypeNames.Application.Pdf));
+                attachment.ContentDisposition.FileName = "CAPP Report.pdf";
+                email.Attachments.Add(attachment);
+                try
+                {
+                    SmtpClient SMTPServer = new SmtpClient("localhost");
+                    SMTPServer.Send(email);
+                }
+                catch
+                {
+                    return false;
+                }
+                return true;
             }
-            report.ContentDisposition.FileName = "CAPP Report.pdf";
-            email.Attachments.Add(report);
-            SmtpClient SMTPServer = new SmtpClient("127.0.0.1");
-            try
-            {
-                SMTPServer.Send(email);
-            }
-            catch
-            {
-                return false;
-            }
-            return true;
         }
     }
 }
